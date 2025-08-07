297e705f37c8c78c62b14fc930d53938a248c1aa
lbianco11197 <luca.bianco@euroirte.it>
2025-08-07 12:21:27 +0200

    Update streamlit_delivery_report.py

--- streamlit_delivery_report.py
+++ streamlit_delivery_report.py
@@ -117,7 +117,7 @@ for col in ["Resa FTTH", "Resa ≠ FTTH"]:
 
 st.dataframe(
     df_giornaliero.style
-    .applymap(lambda v: "background-color: #d4f4dd" if pd.notna(v) and v >= 70 else ("background-color: #f4cccc" if pd.notna(v) and v < 70 else ""), subset=["Resa FTTH", "Resa ≠ FTTH"]),
+    .applymap(lambda v: "background-color: #ccffcc if pd.notna(v) and v >= 70 else ("background-color: #ff9999" if pd.notna(v) and v < 70 else ""), subset=["Resa FTTH", "Resa ≠ FTTH"]),
     use_container_width=True
 )
 
@@ -131,6 +131,6 @@ for col in ["Resa FTTH", "Resa ≠ FTTH"]:
 
 st.dataframe(
     df_mensile.style
-    .applymap(lambda v: "background-color: #d4f4dd" if pd.notna(v) and v >= 70 else ("background-color: #f4cccc" if pd.notna(v) and v < 70 else ""), subset=["Resa FTTH", "Resa ≠ FTTH"]),
+    .applymap(lambda v: "background-color: #ccffcc" if pd.notna(v) and v >= 70 else ("background-color: #ff9999" if pd.notna(v) and v < 70 else ""), subset=["Resa FTTH", "Resa ≠ FTTH"]),
     use_container_width=True
 )
